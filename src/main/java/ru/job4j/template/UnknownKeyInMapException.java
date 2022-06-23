34e80eda6e7783cac9c4ef5d7dbeced72b45f394
Danil Selyutin <d0737@yandex.ru>
2022-06-23 12:15:42 +0300

    3. Шаблонизатор. [#855 #314391]

---
+++ src/main/java/ru/job4j/template/UnknownKeyInMapException.java
@@ -0,0 +1,4 @@
+package ru.job4j.template;
+
+public class UnknownKeyInMapException extends Throwable {
+}
---
+++ src/main/java/ru/job4j/template/UnknownKeyInTemplateException.java
@@ -0,0 +1,4 @@
+package ru.job4j.template;
+
+public class UnknownKeyInTemplateException extends Throwable {
+}
