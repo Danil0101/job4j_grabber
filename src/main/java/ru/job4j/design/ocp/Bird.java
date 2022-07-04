67f7f4cc97abc0946ab82e35a0f7f30af6744c44
Danil Selyutin <d0737@yandex.ru>
2022-07-04 08:13:59 +0300

    0. Принцип открытости закрытости [#4914]

---
+++ src/main/java/ru/job4j/design/ocp/Bird.java
@@ -0,0 +1,7 @@
+package ru.job4j.design.ocp;
+
+public class Bird {
+    void fly() {
+
+    }
+}
---
+++ src/main/java/ru/job4j/design/ocp/Headphones.java
@@ -0,0 +1,5 @@
+package ru.job4j.design.ocp;
+
+public class Headphones {
+    private double wireLength;
+}
