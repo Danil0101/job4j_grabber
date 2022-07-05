87d3bf562a2692133362686d534ce35dfa135014
Danil Selyutin <d0737@yandex.ru>
2022-07-05 10:00:17 +0300

    0. Принцип единственной ответственности [#4913 #318263]

--- src/main/java/ru/job4j/srp/Phone.java
+++ src/main/java/ru/job4j/srp/Phone.java
@@ -1,7 +1,50 @@
 package ru.job4j.srp;
 
+import java.util.Objects;
+
 public class Phone {
-    Phone buyPhone() {
+    private String model;
+    private int partNumber;
+
+    public Phone(String model, int partNumber) {
+        this.model = model;
+        this.partNumber = partNumber;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getPartNumber() {
+        return partNumber;
+    }
+
+    public void setPartNumber(int partNumber) {
+        this.partNumber = partNumber;
+    }
+
+    public Phone buyPhone() {
         return null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Phone phone = (Phone) o;
+        return partNumber == phone.partNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(partNumber);
     }
 }
